f3979780c29256bd19b3a9f8fd9ab1b3f2bb4d31
ZJC009 <73008002+ZJC009@users.noreply.github.com>
2021-07-06 17:52:54 +0900

    Add files via upload

---
+++ 2021.07.cpp
@@ -0,0 +1,102 @@
+﻿#include<stdio.h>
+
+/*
+------ 1 ------
+*/
+//struct Vector2
+//{
+//private:
+//public:
+//    int x;
+//    int y;
+//};
+//struct Vector2 v;
+//int main()
+//{
+//    v.x = 2;
+//    v.y = 2;
+//    printf("struct v: %d, %d\n", v.x, v.y);
+//
+//    return 0;
+//}
+
+/*
+------ 2 ------
+*/
+//struct Vector2
+//{
+//public:
+//    void setX(int x1)
+//    {
+//        x = x1;
+//    }
+//    void setY(int y1)
+//    {
+//        y = y1;
+//    }
+//    int getX()
+//    {
+//        return x;
+//    }
+//    int getY()
+//    {
+//        return y;
+//    }
+//private:
+//    int x;
+//    int y;
+//};
+//struct Vector2 v;
+//int main()
+//{
+//    v.setX(3);
+//    v.setY(3);
+    //printf("struct v: %d, %d\n", v.getX(), v.getY());
+//        return 0;
+//}
+
+/*
+------ 3 ------
+*/
+struct Vector2
+{
+public:
+    void setX(int x1)
+    {
+        x = x1;
+    }
+    void setY(int y1)
+    {
+        y = y1;
+    }
+    void setZ(int z1)
+    {
+        y = z1;
+    }
+    int getX()
+    {
+        return x;
+    }
+    int getY()
+    {
+        return y;
+    }
+    int getZ()
+    {
+        return z;
+    }
+private:
+    int x;
+    int y;
+    int z;
+};
+struct Vector2 v;
+int main()
+{
+    v.setX(3);
+    v.setY(3);
+    v.setZ(3);
+    printf("struct v: %d, %d\n", v.getX(), v.getY(), v.getZ());
+    return 0;
+}
+
